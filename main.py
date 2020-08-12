6e7170fed4845ddc7817ae21792e843bb38c4c3a
owolabioromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2020-08-12 09:19:29 +0100

    Refactor

--- main.py
+++ main.py
@@ -2,97 +2,89 @@ import time, random, copy
 
 class Board:
     def __init__(self):
-        self.L = 100
-        self.grid = ['0' for i in range(self.L)]
-        self.W = 10
-
-
+        self.grid_2d = None
+        self.grid = None
+        self.width = None
+        self.len = None
 
 
     def load_state(self,path):
+        """Load automata state from file and run simulation"""
         with open(path,'r') as f:
-            grid= [list(line.strip()) for line in f.readlines()]
-            self.grid = [j for i in grid for j in i]
+            #get 1d self.grid from 2d txt representation
+            self.grid_2d= [list(line.strip()) for line in f.readlines()]
+            self.grid = [j for i in self.grid_2d for j in i]
 
-        self.W = len(grid[0])
-        self.L = len(self.grid)
+        self.width = len(self.grid_2d[0])
+        self.len = len(self.grid)
         self.loop()
-          
-
-            
 
 
+    def render(self):
+        for row in self.grid_2d:
+            print(" ".join(row))
+        print('\n')
 
 
-    def printArr(self, arr, w, l, flat=True):
-        if flat:
-            str_self = ''
-            for idx in range(l):
-                str_self = str_self + ' ' + arr[idx]
-
-                if idx in range(w-1,l,w):
-                    str_self = str_self + '\n'
-
-            print(str_self)
-
-        else:
-            for a in arr:
-                print(str(a))
-        
+    def refresh_2d(self):
+        """Rebuild the 2d grid representation after 1d update"""
+        self.grid_2d= [[]]
+        y = 0
+        for idx in range(self.len):
+            if idx in range(self.width, self.len, self.width): #crete new row 
+                y += 1
+                self.grid_2d.append([])
+            self.grid_2d[y].append(self.grid[idx])
 
 
     def make_random_state(self):
-        num = random.choice(range(self.L))
-        for i in range(num):
-            idx = random.choice(range(self.L))
+        """Generate a random state and watch it change"""
+
+        self.width = random.choice(range(5,30))
+        height = random.choice(range(5,20))
+        self.len = self.width*height
+
+        for i in range(self.len):
+            idx = random.choice(range(self.len))
             self.grid[idx] = '1'
-            time.sleep(0.7)
-            self.printArr(self.grid, self.W, self.L)
 
         print('State Created... Initializing loop')
-        
         self.loop()
 
 
     def get_neighbors(self, idx):
-        #conv to 2d
-        y = idx // self.W
-        x = idx - y*self.W
-        idx_2d = y,x
+        """Get neighbors of self.grid at index """
 
-
-        new_ls = [[]]
-        count = 0
-        for idx in range(self.L):
-            if idx in range(self.W,self.L,self.W):
-                count += 1
-                new_ls.append([])
-            new_ls[count].append(self.grid[idx])
-
-
-
+        # convert flattened array index to 2d index
+        y = idx // self.width
+        x = idx - y*self.width
+       
+        #get neightbour values and total value of neighbors
         neighbor_vals = []
+        neighbor_index = [[y,x-1],[y,x+1],[y+1,x],
+                            [y+1,x-1],[y+1,x+1],[y-1,x],
+                            [y-1,x-1],[y-1,x+1]]
 
-        n_idxs = [[y,x-1],[y,x+1],[y+1,x],[y+1,x-1],[y+1,x+1],[y-1,x],[y-1,x-1],[y-1,x+1]]
-
-        for idx in n_idxs:
-            y,x = idx
+        for neighbor in neighbor_index:
+            y,x = neighbor
             try:
-                neighbor_vals.append(new_ls[y][x])
-            except:
-                pass
-
+                neighbor_vals.append(self.grid_2d[y][x])
+            except IndexError as e:
+                pass #for edge and corner blocks
 
         neighbor_count = len([i for i in neighbor_vals if i=='1'])
         return neighbor_count
 
 
     def update(self):
+        """Move to next game state"""
 
-        grid = copy.deepcopy(self.grid)
+        grid = self.grid[:] #copy grid, prevent altering game state during state change
 
-        for idx in range(self.L):
+        for idx in range(self.len):
             neighbor_count = self.get_neighbors(idx)
+
+            #game update rules
             if neighbor_count in [0,1] and grid[idx]=='1': grid[idx] = '0'
             elif neighbor_count in [2,3] and grid[idx] == '1': pass 
             elif neighbor_count > 3 and grid[idx] == '1': grid[idx] = '0'
@@ -100,26 +92,19 @@ class Board:
             else : pass
 
 
-        self.grid = copy.deepcopy(grid)
+        self.grid = grid
+        self.refresh_2d()
         
 
-
-
-
     def loop(self):
+        """Watch updates"""
+
         while True:
-            self.printArr(self.grid, self.W, self.L)
+            self.render()
             time.sleep(0.7)
             self.update()
 
 
-
-
-
-
-
 if __name__ == "__main__":
     b = Board()
-    # b.make_random_state()
-    # b.load_state('./toad.txt')
-    b.load_state('./ggg.txt')
+    b.load_state('./blinker.txt')
